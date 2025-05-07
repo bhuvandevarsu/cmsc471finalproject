cc23985f0024cb560f6df144e822f9daaffd49f6
bhuvandevarsu <bhuvandevarsu@gmail.com>
2025-05-06 21:26:53 -0400

    Added Play/Pause buttons.

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 d3.csv("data/public_schools_short.csv", d => ({
     lon: +d.LON,
     lat: +d.LAT
-  })).then(rawData => {
-    const width = 800,
-          height = 600,
-          svg = d3.select("svg"),
-          kInput = d3.select("#k"),
-          resetBtn = d3.select("#reset"),
-          nextBtn = d3.select("#next");
-    
+})).then(rawData => {
+    const width   = 800,
+          height  = 600,
+          svg     = d3.select("svg"),
+          kInput  = d3.select("#k"),
+          resetBtn= d3.select("#reset"),
+          nextBtn = d3.select("#next"),
+          playBtn = d3.select("#play"),
+          pauseBtn= d3.select("#pause");
+
     const projection = d3.geoAlbersUsa()
                          .translate([width/2, height/2])
                          .scale(1000);
@@ -16,25 +18,26 @@ d3.csv("data/public_schools_short.csv", d => ({
     const data = rawData
       .map(d => {
         const xy = projection([d.lon, d.lat]);
-        if (!xy) return null;
-        return { x: xy[0], y: xy[1] };
+        return xy ? { x: xy[0], y: xy[1] } : null;
       })
       .filter(d => d);
 
-    let centroids;
+    let centroids,
+        intervalId = null,
+        intervalDelay = 1000;
 
     function init() {
       const k = +kInput.property("value");
       centroids = d3.shuffle(data)
                     .slice(0, k)
                     .map(d => ({ x: d.x, y: d.y }));
-      step();
+      draw();  
     }
 
     function step() {
       data.forEach(p => {
         let best = 0, bestDist = Infinity;
-        centroids.forEach((c, i) => {
+        centroids.forEach((c,i) => {
           const dx = p.x - c.x,
                 dy = p.y - c.y,
                 dist = dx*dx + dy*dy;
@@ -45,7 +48,6 @@ d3.csv("data/public_schools_short.csv", d => ({
         });
         p.cluster = best;
       });
-
       centroids = centroids.map((c,i) => {
         const members = data.filter(p => p.cluster === i);
         return {
@@ -53,34 +55,57 @@ d3.csv("data/public_schools_short.csv", d => ({
           y: d3.mean(members, p => p.y)
         };
       });
-
       draw();
     }
 
     function draw() {
       const color = d3.schemeCategory10;
-
       svg.selectAll("circle.point")
          .data(data)
          .join("circle")
-         .attr("class","point")
-         .attr("r",3)
-         .attr("cx", d=>d.x)
-         .attr("cy", d=>d.y)
-         .attr("fill", d=>color[d.cluster]);
+           .attr("class","point")
+           .attr("r",3)
+           .attr("cx", d=>d.x)
+           .attr("cy", d=>d.y)
+           .attr("fill", d=>color[d.cluster]);
 
       svg.selectAll("circle.centroid")
          .data(centroids)
          .join("circle")
-         .attr("class","centroid")
-         .attr("r",8)
-         .attr("cx", d=>d.x)
-         .attr("cy", d=>d.y)
-         .attr("fill", (_,i)=>color[i]);
+           .attr("class","centroid")
+           .attr("r",8)
+           .attr("cx", d=>d.x)
+           .attr("cy", d=>d.y)
+           .attr("fill", (_,i)=>color[i]);
     }
 
-    resetBtn.on("click", init);
-    nextBtn.on("click", step);
-    
+    resetBtn.on("click", () => {
+      stopAuto();
+      init();
+    });
+    nextBtn.on("click", () => {
+      stopAuto();
+      step();
+    });
+
+    playBtn.on("click", () => {
+      if (intervalId) return;
+      intervalId = setInterval(step, intervalDelay);
+      playBtn.attr("disabled", true);
+      pauseBtn.attr("disabled", null);
+    });
+    pauseBtn.on("click", () => {
+      stopAuto();
+    });
+
+    function stopAuto() {
+      if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+      }
+      playBtn.attr("disabled", null);
+      pauseBtn.attr("disabled", true);
+    }
+
     init();
-  });
+});
